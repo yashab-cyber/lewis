685d20468927d7948673ff69f9f4961768e0808b
Yashab Alam <77148571+yashab-cyber@users.noreply.github.com>
2025-03-30 06:46:48 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,17 @@
-hi
+js_content = '''
+function executeCommand() {
+    const command = document.getElementById("command").value;
+    fetch("/execute", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ command: command })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("output").innerText = data.output;
+    });
+}
+'''
+
+with open("static/script.js", "w") as f:
+    f.write(js_content)
