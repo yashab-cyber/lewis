efd71b711a6ac4b6b719b093b8dd0b1640fa0ddd
Yashab Alam <77148571+yashab-cyber@users.noreply.github.com>
2025-04-25 14:54:27 +0530

    Delete lewis_cyber_law_audit directory

--- lewis_cyber_law_audit/modules/law_audit/rules/gdpr.py
+++
@@ -1,8 +0,0 @@
-def get_gdpr_rules():
-    return [
-        'Article 5 - Principles Relating to Processing',
-        'Article 6 - Lawfulness of Processing',
-        'Article 32 - Security of Processing',
-        'Article 33 - Notification of a Personal Data Breach',
-        'Article 35 - Data Protection Impact Assessment'
-    ]
